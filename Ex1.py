1e51b369d482b794ffa9281b5fd3305bf0ebebb2
GustavoBicaletto <bicalettogustavo@hotmail.com>
2023-05-28 20:28:07 -0300

    Mudanças feitas com jaiminho e thiago pouca pika
    muda

---
+++ Ex1.py
@@ -0,0 +1,50 @@
+# 1 - Elabore uma função que dado um número inteiro ela imprima o seu sucessor e antecessor
+
+    # control + 2 pontos = comentario
+
+'''def numero(numero_selec):    
+    print(f'O antecessor de {numero_selec} é {numero_selec-1}')
+    print(f'O sucessor de {numero_selec} é {numero_selec+1}')
+
+x = int(input('Digite um número: '))
+numero(x) 
+
+def numero():    
+    x = int(input('Digite um número: '))
+    print(f'O antecessor de {x} é {x-1}')
+    print(f'O sucessor de {x} é {x+1}')
+    
+numero()  '''
+
+# JEITOS DIFERENTES PRO MESMO RESULTADO
+
+''' 2 - Elabore um programa que possua duas funções:
+• Uma função que leia um número inteiro
+• Uma função que some três valores inteiros
+• O programa deverá ler três valores inteiros e imprimir a soma
+desses valores, usando as funções acima '''
+
+'''def numero_int():
+    x = int(input('Digite um numero inteiro: '))
+    return x
+
+resultado = numero_int()
+print(resultado)'''
+
+# TODA FUNÇÃO TEM UM RETURN
+
+def numero_int(): 
+    numero = int(input('Digite um numero inteiro: '))
+    print(numero)
+    return numero
+
+def tres_int(a,b,c):
+    soma = a+b+c
+    #return soma
+    print(soma)
+
+a = numero_int()
+b = numero_int()
+c = numero_int()
+
+tres_int(a,b,c)
